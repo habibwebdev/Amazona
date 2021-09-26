06cc5243c4af989deb6707e8395a831d34a0c3ba
habibwebdev <abbasali211@gmail.com>
2021-09-26 15:25:07 +0500

    product routes setup

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+// import { Head } from 'next/head'
+import Head from 'next/head'
+import NextLink from 'next/link'
+import useStyles from '../utils/styles'
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core'
+
+export default function Layout({ children }) {
+  const classes = useStyles()
+  return (
+    <div>
+      <Head>
+        <title>Next Amazona</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart">Cart</NextLink>
+            <NextLink href="/login">Login</NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All right reserved. Next Amazona.</Typography>
+      </footer>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  // For serverside rendering of material ui Elements in conjuction of creation of _document.js file
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
