6508c95cfab3013215bbde15fab484b538313bba
habibwebdev <abbasali211@gmail.com>
2021-10-01 21:32:17 +0500

    User API Built

--- pages/login.js
+++ pages/login.js
@@ -6,16 +6,30 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import useStyles from './../utils/styles'
 import NextLink from 'next/link'
+import axios from 'axios'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const classes = useStyles()
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('/api/users/login', { email, password })
+      alert('success login')
+    } catch (error) {
+      alert(error.response.data ? error.response.data.message : error.message)
+    }
+  }
   return (
     <Layout>
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -27,6 +41,7 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -36,6 +51,7 @@ export default function Login() {
               id="password"
               label="Password"
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
