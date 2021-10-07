9a0fe85a833a571b76e1260c7e3a5f10b564ed62
habibwebdev <abbasali211@gmail.com>
2021-10-07 05:28:15 +0500

    Form Validations applied to Login and Register Forms

--- pages/register.js
+++ pages/register.js
@@ -14,8 +14,16 @@ import axios from 'axios'
 import { Store } from '../utils/Store'
 import { useRouter } from 'next/router'
 import Cookies from 'js-cookie'
+import { Controller, useForm } from 'react-hook-form'
+import { useSnackbar } from 'notistack'
 
 export default function Register() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm()
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
   const router = useRouter()
   const { redirect } = router.query
   const { state, dispatch } = useContext(Store)
@@ -27,17 +35,20 @@ export default function Register() {
     }
   }, [])
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  //   const [name, setName] = useState('')
+  //   const [email, setEmail] = useState('')
+  //   const [password, setPassword] = useState('')
+  //   const [confirmPassword, setConfirmPassword] = useState('')
 
   const classes = useStyles()
 
-  const submitHandler = async (e) => {
-    e.preventDefault()
+  //   const submitHandler = async (e) => {
+  const submitHandler = async ({ name, email, password, confirmPassword }) => {
+    closeSnackbar()
+    // e.preventDefault()
     if (password !== confirmPassword) {
-      alert('Passwords do not match!')
+      //   alert('Passwords do not match!')
+      enqueueSnackbar("Passwords don't match!", { variant: 'error' })
       return
     }
     try {
@@ -52,55 +63,139 @@ export default function Register() {
       router.push(redirect || '/')
       //   alert('success login')
     } catch (error) {
-      alert(error.response.data ? error.response.data.message : error.message)
+      //   alert(error.response.data ? error.response.data.message : error.message)
+      enqueueSnackbar(
+        error.response.data ? error.response.data.message : error.message,
+        { variant: 'error' }
+      )
     }
   }
   return (
     <Layout title="Register">
-      <form onSubmit={submitHandler} className={classes.form}>
+      <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
         <Typography component="h1" variant="h1">
           Register
         </Typography>
         <List>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="name"
-              label="Name"
-              inputProps={{ type: 'name' }}
-              onChange={(e) => setName(e.target.value)}
-            ></TextField>
+            <Controller
+              name="name"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="name"
+                  label="Name"
+                  inputProps={{ type: 'name' }}
+                  //   onChange={(e) => setName(e.target.value)}
+                  error={Boolean(errors.name)}
+                  helperText={
+                    errors.name
+                      ? errors.name.type === 'minLength'
+                        ? 'Name length is more than 1'
+                        : 'Name is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="email"
-              label="Email"
-              inputProps={{ type: 'email' }}
-              onChange={(e) => setEmail(e.target.value)}
-            ></TextField>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  inputProps={{ type: 'email' }}
+                  // onChange={(e) => setEmail(e.target.value)}
+                  error={Boolean(errors.email)}
+                  helperText={
+                    errors.email
+                      ? errors.email.type === 'pattern'
+                        ? 'Email is not valid'
+                        : 'Email is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="password"
-              label="Password"
-              inputProps={{ type: 'password' }}
-              onChange={(e) => setPassword(e.target.value)}
-            ></TextField>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 6,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  inputProps={{ type: 'password' }}
+                  // onChange={(e) => setPassword(e.target.value)}
+                  error={Boolean(errors.password)}
+                  helperText={
+                    errors.password
+                      ? errors.password.type === 'minLength'
+                        ? 'Password length is more than 5'
+                        : 'Password is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="confirmPassword"
-              label="Confirm Password"
-              inputProps={{ type: 'password' }}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            ></TextField>
+            <Controller
+              name="confirmPassword"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 6,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="confirmPassword"
+                  label="Confirm Password"
+                  inputProps={{ type: 'password' }}
+                  //   onChange={(e) => setConfirmPassword(e.target.value)}
+                  error={Boolean(errors.confirmPassword)}
+                  helperText={
+                    errors.confirmPassword
+                      ? errors.confirmPassword.type === 'minLength'
+                        ? 'Confirm Password length is more than 5'
+                        : 'Confirm Password is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
