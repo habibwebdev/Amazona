8b27e8358061253a96b0fc71de15cf19a07acbff
habibwebdev <abbasali211@gmail.com>
2021-10-10 15:40:10 +0500

    Order History Screen created

--- components/Layout.js
+++ components/Layout.js
@@ -63,8 +63,11 @@ export default function Layout({ title, children, description }) {
     setAnchorEl(e.currentTarget)
   }
 
-  const loginMenuCloseHandler = () => {
+  const loginMenuCloseHandler = (e, redirect) => {
     setAnchorEl(null)
+    if (redirect) {
+      router.push(redirect)
+    }
   }
 
   const logoutClickHandler = () => {
@@ -126,9 +129,17 @@ export default function Layout({ title, children, description }) {
                     open={Boolean(anchorEl)}
                     onClose={loginMenuCloseHandler}
                   >
-                    <MenuItem onClick={loginMenuCloseHandler}>Profile</MenuItem>
-                    <MenuItem onClick={loginMenuCloseHandler}>
-                      My account
+                    <MenuItem
+                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+                    >
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) =>
+                        loginMenuCloseHandler(e, '/order-history')
+                      }
+                    >
+                      Order History
                     </MenuItem>
                     <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
                   </Menu>
