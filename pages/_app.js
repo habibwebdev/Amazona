e38a13f176687f83814d8dc54a5b2fa42c1624cc
habibwebdev <abbasali211@gmail.com>
2021-10-19 20:33:23 +0500

    Correction for serverside rendering of material ui styles

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   // For serverside rendering of material ui Elements in conjuction of creation of _document.js file
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
+    if (jssStyles && jssStyles.parentElement) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
   }, [])
