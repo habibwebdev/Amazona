53ee5355edc3d33765bf7fd51df66729e7eb622f
habibwebdev <abbasali211@gmail.com>
2021-10-23 23:15:26 +0500

    navbar class and html ssr rendering on homepage fixed

--- components/Layout.js
+++ components/Layout.js
@@ -37,6 +37,12 @@ import { useSnackbar } from 'notistack'
 import axios from 'axios'
 
 export default function Layout({ title, children, description }) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   const router = useRouter()
   const { state, dispatch } = useContext(Store)
   const { darkMode, cart, userInfo } = state
@@ -66,6 +72,7 @@ export default function Layout({ title, children, description }) {
   })
   const classes = useStyles()
 
+  const [navbar, setNavbar] = useState('')
   const [sidbarVisible, setSidebarVisible] = useState(false)
   const sidebarOpenHandler = () => {
     setSidebarVisible(true)
@@ -97,6 +104,8 @@ export default function Layout({ title, children, description }) {
 
   useEffect(() => {
     fetchCategories()
+    setNavbar(classes.navbar)
+    // console.log(classes.navbar)
   }, [])
 
   const darkModeChangeHandler = () => {
@@ -125,163 +134,172 @@ export default function Layout({ title, children, description }) {
     router.push('/')
   }
   return (
-    <div>
-      <Head>
-        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
-        {description && <meta name="description" content={description}></meta>}
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AppBar position="static" className={classes.navbar}>
-          <Toolbar className={classes.toolbar}>
-            <Box display="flex" alignItems="center">
-              <IconButton
-                edge="start"
-                aria-label="open drawer"
-                onClick={sidebarOpenHandler}
-                className={classes.menuButton}
-              >
-                <MenuIcon className={classes.navbarButton} />
-              </IconButton>
-              <NextLink href="/" passHref>
-                <Link>
-                  <Typography className={classes.brand}>amazona</Typography>
-                </Link>
-              </NextLink>
-            </Box>
-            <Drawer
-              anchor="left"
-              open={sidbarVisible}
-              onClose={sidebarCloseHandler}
-            >
-              <List>
-                <ListItem>
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="space-between"
-                  >
-                    <Typography>Shopping by category</Typography>
-                    <IconButton
-                      aria-label="close"
-                      onClick={sidebarCloseHandler}
-                    >
-                      <CancelIcon />
-                    </IconButton>
-                  </Box>
-                </ListItem>
-                <Divider light />
-                {categories.map((category) => (
-                  <NextLink
-                    key={category}
-                    href={`/search?category=${category}`}
-                    passHref
-                  >
-                    <ListItem
-                      button
-                      component="a"
-                      onClick={sidebarCloseHandler}
-                    >
-                      <ListItemText primary={category}></ListItemText>
-                    </ListItem>
-                  </NextLink>
-                ))}
-              </List>
-            </Drawer>
-            <div className={classes.searchSection}>
-              <form onSubmit={submitHandler} className={classes.searchForm}>
-                <InputBase
-                  name="query"
-                  className={classes.searchInput}
-                  placeholder="Search products"
-                  onChange={queryChangeHandler}
-                />
+    mounted && (
+      <div>
+        <Head>
+          <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+          {description && (
+            <meta name="description" content={description}></meta>
+          )}
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppBar
+            position="static"
+            // className={classes.navbar}
+            // style={{ background: '#2E3B55' }}
+            className={navbar}
+          >
+            <Toolbar className={classes.toolbar}>
+              <Box display="flex" alignItems="center">
                 <IconButton
-                  type="submit"
-                  className={classes.iconButton}
-                  aria-label="search"
+                  edge="start"
+                  aria-label="open drawer"
+                  onClick={sidebarOpenHandler}
+                  className={classes.menuButton}
                 >
-                  <SearchIcon />
+                  <MenuIcon className={classes.navbarButton} />
                 </IconButton>
-              </form>
-            </div>
-            <div>
-              <Switch
-                checked={darkMode}
-                onChange={darkModeChangeHandler}
-              ></Switch>
-              <NextLink href="/cart" passHref>
-                <Link>
-                  <Typography component="span">
-                    {cart.cartItems.length > 0 ? (
-                      <Badge
-                        color="secondary"
-                        badgeContent={cart.cartItems.length}
-                      >
-                        Cart
-                      </Badge>
-                    ) : (
-                      'Cart'
-                    )}
-                  </Typography>
-                </Link>
-              </NextLink>
-              {userInfo ? (
-                <>
-                  <Button
-                    aria-controls="simple-menu"
-                    aria-haspopup="true"
-                    onClick={loginClickHandler}
-                    className={classes.navbarButton}
-                  >
-                    {userInfo.name}
-                  </Button>
-                  <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={loginMenuCloseHandler}
-                  >
-                    <MenuItem
-                      onClick={(e) => loginMenuCloseHandler(e, '/profile')}
-                    >
-                      Profile
-                    </MenuItem>
-                    <MenuItem
-                      onClick={(e) =>
-                        loginMenuCloseHandler(e, '/order-history')
-                      }
-                    >
-                      Order History
-                    </MenuItem>
-                    {userInfo.isAdmin && (
-                      <MenuItem
-                        onClick={(e) =>
-                          loginMenuCloseHandler(e, '/admin/dashboard')
-                        }
-                      >
-                        Admin Dashboard
-                      </MenuItem>
-                    )}
-                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
-                  </Menu>
-                </>
-              ) : (
-                <NextLink href="/login" passHref>
+                <NextLink href="/" passHref>
                   <Link>
-                    <Typography component="span">Login</Typography>
+                    <Typography className={classes.brand}>amazona</Typography>
                   </Link>
                 </NextLink>
-              )}
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Container className={classes.main}>{children}</Container>
-        <footer className={classes.footer}>
-          <Typography>All right reserved. Next Amazona.</Typography>
-        </footer>
-      </ThemeProvider>
-    </div>
+              </Box>
+              <Drawer
+                anchor="left"
+                open={sidbarVisible}
+                onClose={sidebarCloseHandler}
+              >
+                <List>
+                  <ListItem>
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="space-between"
+                    >
+                      <Typography>Shopping by category</Typography>
+                      <IconButton
+                        aria-label="close"
+                        onClick={sidebarCloseHandler}
+                      >
+                        <CancelIcon />
+                      </IconButton>
+                    </Box>
+                  </ListItem>
+                  <Divider light />
+                  {categories.map((category) => (
+                    <NextLink
+                      key={category}
+                      href={`/search?category=${category}`}
+                      passHref
+                    >
+                      <ListItem
+                        button
+                        component="a"
+                        onClick={sidebarCloseHandler}
+                      >
+                        <ListItemText primary={category}></ListItemText>
+                      </ListItem>
+                    </NextLink>
+                  ))}
+                </List>
+              </Drawer>
+              <div className={classes.searchSection}>
+                <form onSubmit={submitHandler} className={classes.searchForm}>
+                  <InputBase
+                    name="query"
+                    className={classes.searchInput}
+                    placeholder="Search products"
+                    onChange={queryChangeHandler}
+                  />
+                  <IconButton
+                    type="submit"
+                    className={classes.iconButton}
+                    aria-label="search"
+                  >
+                    <SearchIcon />
+                  </IconButton>
+                </form>
+              </div>
+              <div>
+                <Switch
+                  checked={darkMode}
+                  onChange={darkModeChangeHandler}
+                ></Switch>
+                <NextLink href="/cart" passHref>
+                  <Link>
+                    <Typography component="span">
+                      {cart.cartItems.length > 0 ? (
+                        <Badge
+                          color="secondary"
+                          badgeContent={cart.cartItems.length}
+                        >
+                          Cart
+                        </Badge>
+                      ) : (
+                        'Cart'
+                      )}
+                    </Typography>
+                  </Link>
+                </NextLink>
+                {userInfo ? (
+                  <>
+                    <Button
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      onClick={loginClickHandler}
+                      className={classes.navbarButton}
+                    >
+                      {userInfo.name}
+                    </Button>
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={loginMenuCloseHandler}
+                    >
+                      <MenuItem
+                        onClick={(e) => loginMenuCloseHandler(e, '/profile')}
+                      >
+                        Profile
+                      </MenuItem>
+                      <MenuItem
+                        onClick={(e) =>
+                          loginMenuCloseHandler(e, '/order-history')
+                        }
+                      >
+                        Order History
+                      </MenuItem>
+                      {userInfo.isAdmin && (
+                        <MenuItem
+                          onClick={(e) =>
+                            loginMenuCloseHandler(e, '/admin/dashboard')
+                          }
+                        >
+                          Admin Dashboard
+                        </MenuItem>
+                      )}
+                      <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+                    </Menu>
+                  </>
+                ) : (
+                  <NextLink href="/login" passHref>
+                    <Link>
+                      <Typography component="span">Login</Typography>
+                    </Link>
+                  </NextLink>
+                )}
+              </div>
+            </Toolbar>
+          </AppBar>
+          <Container className={classes.main}>{children}</Container>
+          <footer className={classes.footer}>
+            <Typography>All right reserved. Next Amazona.</Typography>
+          </footer>
+        </ThemeProvider>
+      </div>
+    )
   )
 }
